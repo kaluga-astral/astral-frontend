34f9d626827d3ad945084160c07939222c172eea
Pavel Perevezentsev <perevezenzev@gmail.com>
2018-09-17 21:19:12 +0300

    states in table

--- components/Table/TableBody.jsx
+++ components/Table/TableBody.jsx
@@ -6,31 +6,51 @@ import Loader from '../status/Loader';
 import TableRow from './TableRow';
 import TableCell from './TableCell';
 
-const TableBodyLoading = withStyles({
+const TableBodyStatus = withStyles({
   wrapper: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     padding: '25px',
   },
-})(({ classes }) => (
+})(({ classes, children }) => (
   <MuiTableBody>
     <TableRow>
       <TableCell colSpan={1000}>
-        <div className={classes.wrapper}>
-          <Loader />
-        </div>
+        <div className={classes.wrapper}>{children}</div>
       </TableCell>
     </TableRow>
   </MuiTableBody>
 ));
 
-const TableBody = ({ isFetching, ...props }) => {
+const TableBody = ({
+  isFetching, data, error, renderRow, ...props
+}) => {
   if (isFetching) {
-    return <TableBodyLoading />;
+    return (
+      <TableBodyStatus>
+        <Loader />
+      </TableBodyStatus>
+    );
   }
 
-  return <MuiTableBody {...props} />;
+  if (data.length === 0) {
+    return (
+      <TableBodyStatus>
+        <span>Ничего не найдено</span>
+      </TableBodyStatus>
+    );
+  }
+
+  if (error) {
+    return (
+      <TableBodyStatus>
+        <span>{(error || { response: { data: {} } }).response.data.message}</span>
+      </TableBodyStatus>
+    );
+  }
+
+  return <MuiTableBody {...props}>{data.map(renderRow)}</MuiTableBody>;
 };
 
 export default withStyles({})(TableBody);
