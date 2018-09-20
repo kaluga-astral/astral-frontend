7c10b36924af59239ed539048d7bfd1fbdc60904
Pavel Perevezentsev <perevezenzev@gmail.com>
2018-09-20 15:57:28 +0300

    fix

--- components/Tabs/Tab.jsx
+++ components/Tabs/Tab.jsx
@@ -1,1 +1,18 @@
-export { default } from '@material-ui/core/Tab';
+import React from 'react';
+import MuiTab from '@material-ui/core/Tab';
+import { withStyles } from '@material-ui/core/styles';
+
+const Tab = props => <MuiTab {...props} />;
+
+export default withStyles(theme => ({
+  root: {
+    textTransform: 'initial',
+    fontSize: '14px',
+    fontWeight: 300,
+    color: theme.palette.common.black,
+  },
+  selected: {
+    fontWeight: 500,
+    color: theme.palette.primary.main,
+  },
+}))(Tab);
--- components/Tabs/Tabs.jsx
+++ components/Tabs/Tabs.jsx
@@ -1,1 +1,13 @@
-export { default } from '@material-ui/core/Tabs';
+import React from 'react';
+import MuiTabs from '@material-ui/core/Tabs';
+import { withStyles } from '@material-ui/core/styles';
+
+const Tabs = props => <MuiTabs {...props} />;
+
+Tabs.defaultProps = {
+  indicatorColor: 'primary',
+};
+
+export default withStyles({
+  root: {},
+})(Tabs);
