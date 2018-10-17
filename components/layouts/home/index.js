cbfc5072a958de4ed3e5182e1f3d5dd61b48fdba
Pavel Perevezentsev <perevezenzev@gmail.com>
2018-10-17 11:40:20 +0300

    fix

--- components/layouts/home/index.js
+++ components/layouts/home/index.js
@@ -1,4 +1,5 @@
 export { default as Content } from './Content';
 export { default as Footer } from './Footer';
 export { default } from './Layout';
+export { default as Main } from './Main';
 export { default as Header } from './Header';
