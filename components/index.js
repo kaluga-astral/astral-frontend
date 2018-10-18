f73ab66ab95d049a0469217e79e277d7bef4fda0
Pavel Perevezentsev <perevezenzev@gmail.com>
2018-10-18 14:00:56 +0300

    fix

--- components/index.js
+++ components/index.js
@@ -4,6 +4,7 @@ export { default as Collapse } from './Collapse';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as IconButton } from './IconButton';
 export { default as Loadable } from './Loadable';
+export { default as Link } from './Link';
 export { default as LinkAsButton } from './LinkAsButton';
 export { default as Pagination } from './Pagination';
 export { default as SvgIcon } from './SvgIcon';
