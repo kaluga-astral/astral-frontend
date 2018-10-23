cb0a46d5e28575aeab7d8aeacaa3d16b513de475
Pavel Perevezentsev <perevezenzev@gmail.com>
2018-10-23 15:31:27 +0300

    fix

--- components/List/List.jsx
+++ components/List/List.jsx
@@ -1,21 +1,4 @@
 import MuiList from '@material-ui/core/List';
 import { withStyles } from '@material-ui/core/styles';
 
-import React from 'react';
-
-const TableBodyNotMatchStatus = withStyles({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '20px',
-  },
-})(({ classes }) => <div className={classes.root}>Ничего не найдено</div>);
-
-const List = ({ data, renderItem, ...props }) => (
-  <MuiList {...props}>
-    {data.length === 0 && <TableBodyNotMatchStatus />}
-    {data.map(renderItem)}
-  </MuiList>
-);
-
-export default withStyles({})(List);
+export default withStyles({})(MuiList);
