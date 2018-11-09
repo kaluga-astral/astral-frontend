0c68f9b3f80e68133c8563d22d37808e3af5d03e
merkelAlbert <merkel.albert1997@gmail.com>
2018-11-09 15:00:16 +0300

    dialog title fix

--- components/Dialog/DialogTitle.jsx
+++ components/Dialog/DialogTitle.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import { withStyles } from '@material-ui/core/styles';
+import IconButton from '../IconButton';
 
-const DialogTitle = props => <MuiDialogTitle {...props} />;
+import CloseIcon from './CloseIcon';
+
+const DialogTitle = ({
+  children,
+  onIconClick,
+  ...props
+}) => (
+  <MuiDialogTitle {...props} disableTypography>
+    {children}
+    <IconButton onClick={onIconClick}>
+      <CloseIcon />
+    </IconButton>
+  </MuiDialogTitle>
+);
+
+DialogTitle.defaultProps = {
+  onIconClick: () => { },
+};
+
+DialogTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+  onIconClick: PropTypes.func,
+};
 
 export default withStyles({
   root: {
     borderBottom: '1px solid #ededed',
-    padding: '25px',
+    padding: '32px',
     fontWeight: 300,
     fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 })(DialogTitle);
