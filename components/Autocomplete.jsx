86860416b3cc8ac78e73c13df496adc466c59341
arsenijnikitockin <arseniynikitochkin@ya.ru>
2018-11-19 16:44:21 +0300

    Автокомплиты, Динамические поиски, Контейнер для фильтров, частично фильтры в звонках

--- components/Autocomplete.jsx
+++ components/Autocomplete.jsx
@@ -1,94 +1,232 @@
-import cn from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import Downshift from 'downshift';
+import classNames from 'classnames';
+import Select from 'react-select';
+import { isFunction } from 'lodash-es';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
 import MenuItem from '@material-ui/core/MenuItem';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
 
-class Autocomplete extends PureComponent {
-  renderInput = (InputProps) => {
-    const { classes, renderInput } = this.props;
-
-    if (renderInput) {
-      return renderInput(InputProps);
-    }
-
-    return (
-      <TextField
-        fullWidth
-        InputProps={{
-          classes: {
-            root: classes.inputRoot,
-          },
-          ...InputProps,
-        }}
-      />
-    );
-  };
-
-  renderSuggestions = ({ getItemProps, highlightedIndex }) => {
-    const { classes, items } = this.props;
-
-    return (
-      <Paper className={classes.paper} square>
-        {items.map((item, index) => {
-          const itemProps = getItemProps({ item });
-          const isSelected = highlightedIndex === index;
-
-          return (
-            <MenuItem {...itemProps} key={item.label} selected={isSelected} component="div">
-              {item.label}
-            </MenuItem>
-          );
-        })}
-      </Paper>
-    );
-  };
-
-  render = () => {
-    const {
-      classes, className, placeholder, ...props
-    } = this.props;
-
-    return (
-      <Downshift {...props} itemToString={item => (item ? item.label : '')}>
-        {({ getInputProps, isOpen, ...downshiftState }) => (
-          <div className={cn(classes.root, className)}>
-            {this.renderInput(getInputProps({ placeholder }))}
-            {isOpen && this.renderSuggestions(downshiftState)}
-          </div>
-        )}
-      </Downshift>
-    );
-  };
-}
-
-Autocomplete.defaultProps = {
-  placeholder: null,
-  className: null,
-  renderInput: null,
-};
-
-Autocomplete.propTypes = {
-  placeholder: PropTypes.string,
-  classes: PropTypes.shape({}).isRequired,
-  items: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  className: PropTypes.string,
-  renderInput: PropTypes.func,
-};
-
-export default withStyles(theme => ({
+const styles = theme => ({
   root: {
-    position: 'relative',
     flexGrow: 1,
+    height: 250,
+  },
+  input: {
+    display: 'flex',
+    padding: 0,
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+      0.08,
+    ),
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  singleValue: {
+    fontSize: 16,
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    fontSize: 16,
   },
   paper: {
     position: 'absolute',
-    marginTop: theme.spacing.unit,
     zIndex: 1,
+    marginTop: theme.spacing.unit,
     left: 0,
     right: 0,
   },
-}))(Autocomplete);
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
+});
+
+function NoOptionsMessage(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />;
+}
+
+function Control(props) {
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: props.selectProps.classes.input,
+          inputRef: props.innerRef,
+          children: props.children,
+          ...props.innerProps,
+        },
+      }}
+      {...props.selectProps.textFieldProps}
+    />
+  );
+}
+
+function Option(props) {
+  return (
+    <MenuItem
+      buttonRef={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400,
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+function Placeholder(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.placeholder}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function SingleValue(props) {
+  return (
+    <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+      {props.children}
+    </Typography>
+  );
+}
+
+function ValueContainer(props) {
+  return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+}
+
+function MultiValue(props) {
+  return (
+    <Chip
+      tabIndex={-1}
+      label={props.children}
+      className={classNames(props.selectProps.classes.chip, {
+        [props.selectProps.classes.chipFocused]: props.isFocused,
+      })}
+      onDelete={props.removeProps.onClick}
+    />
+  );
+}
+
+function Menu(props) {
+  return (
+    <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+      {props.children}
+    </Paper>
+  );
+}
+
+const components = {
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+};
+
+class Autocomplete extends React.Component {
+  state = {
+    value: null,
+  };
+
+  handleChange = value => {
+    const { onChange } = this.props;
+    if (isFunction(onChange)) {
+      onChange(value);
+    }
+
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes, theme, options, value, placeholder, label, isMulti, isLoading, isDisabled, onInputChange, onMenuOpen } = this.props;
+
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        '& input': {
+          font: 'inherit',
+        },
+      }),
+    };
+
+    return (
+      <Select
+        classes={classes}
+        styles={selectStyles}
+        options={options}
+        components={components}
+        value={value || this.state.value}
+        placeholder={placeholder}
+        textFieldProps={{ label, InputLabelProps: { shrink: true } }}
+        isMulti={isMulti}
+        isLoading={isLoading}
+        isDisabled={isDisabled}
+        onChange={this.handleChange}
+        onInputChange={onInputChange}
+        onMenuOpen={onMenuOpen}
+      />
+    );
+  }
+}
+
+Autocomplete.defaultProps = {
+  placeholder: 'Выберите значение',
+  isMulti: false,
+  isLoading: false,
+  isDisabled: false
+};
+
+Autocomplete.propTypes = {
+  isMulti: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  value: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  options: PropTypes.array.isRequired,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Autocomplete);
