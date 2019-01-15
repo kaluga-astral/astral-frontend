75acdfe3d2c73315705a2ed8d496326b9e93f4fb
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-01-15 16:55:48 +0300

    fix

--- components/form/fields/base/Checkbox.jsx
+++ components/form/fields/base/Checkbox.jsx
@@ -5,12 +5,16 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '../../../Checkbox';
 import Field from '../Field';
 
-const CheckboxField = ({ label, icon, ...props }) => (
+const CheckboxField = ({
+  label, icon, checkedIcon, ...props
+}) => (
   <Field {...props} type="checkbox">
     {({ input: { value, ...input } }) => (
       <FormControlLabel
         label={label}
-        control={<Checkbox icon={icon} {...input} value={String(value)} />}
+        control={
+          <Checkbox icon={icon} checkedIcon={checkedIcon} {...input} value={String(value)} />
+        }
       />
     )}
   </Field>
