e2b88cd41553020a41d32993ad1e3801832143ab
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-02-20 13:02:59 +0300

    Stepper

---
+++ components/Stepper/index.js
@@ -0,0 +1,9 @@
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+
+Step.Label = StepLabel;
+Step.Content = StepContent;
+
+export { Stepper as default, Step };
