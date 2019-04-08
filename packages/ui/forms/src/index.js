b447baa06d39698f0f01c78c4d7b1310eda69e17
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-04-08 13:56:33 +0300

    form fixes

--- packages/ui/forms/src/index.js
+++ packages/ui/forms/src/index.js
@@ -1,2 +1,4 @@
-/* eslint-disable import/prefer-default-export */
+export { default as ErrorAlert } from './ErrorAlert';
 export { default as Form } from './Form';
+export { default as SubmitButton } from './SubmitButton';
+export { default as TextField } from './TextField';
