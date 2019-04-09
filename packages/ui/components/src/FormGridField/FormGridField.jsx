05341b3247ffdf7efb524882c0239334de29af97
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-04-09 14:36:23 +0300

    FormGrid

---
+++ packages/ui/components/src/FormGridField/FormGridField.jsx
@@ -0,0 +1,30 @@
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { withStyles } from '@astral-frontend/styles';
+
+const FormGridField = ({
+  classes, className, component: Component, ...props
+}) => (
+  <Component className={cn(classes.root, className)} {...props} />
+);
+
+FormGridField.defaultProps = {
+  className: null,
+};
+
+FormGridField.propTypes = {
+  className: PropTypes.string,
+  component: PropTypes.func.isRequired,
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles({
+  root: {
+    flex: 1,
+
+    '&:not(:last-child)': {
+      marginRight: '20px',
+    },
+  },
+})(FormGridField);
---
+++ packages/ui/components/src/FormGridRow/FormGridRow.jsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { withStyles } from '@astral-frontend/styles';
+
+const FormGridRow = ({
+  classes, className, children, ...props
+}) => (
+  <div {...props} className={cn(classes.root, className)}>
+    {children}
+  </div>
+);
+
+FormGridRow.defaultProps = {
+  className: null,
+};
+
+FormGridRow.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles({
+  root: {
+    display: 'flex',
+  },
+})(FormGridRow);
