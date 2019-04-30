edd9cac35e5984dd9e47262ed8c4da5f40641d80
Maxim <isaevmaxim1905@gmail.com>
2019-04-30 16:33:23 +0300

    Поле выбор организации

--- packages/ui/icons/src/index.js
+++ packages/ui/icons/src/index.js
@@ -3,3 +3,4 @@ export { default as EditIcon } from './EditIcon';
 export { default as SearchIcon } from './SearchIcon';
 export { default as OrganizationIcon } from './OrganizationIcon';
 export { default as ArrowIcon } from './ArrowIcon';
+export { default as OrganizationNotFoundIcon } from './OrganizationNotFoundIcon';
