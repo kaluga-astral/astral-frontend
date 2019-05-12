51f4d1abf0219c7775bdd2ee03d41a114243e3c0
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-05-12 22:08:17 +0300

    fix

---
+++ packages/ui/dashboard-layout/src/ContentNavFilters/index.js
@@ -0,0 +1,6 @@
+import ContentNavFilters from './ContentNavFilters';
+import ContentNavFiltersItem from './ContentNavFiltersItem';
+
+ContentNavFilters.Item = ContentNavFiltersItem;
+
+export default ContentNavFilters;
