b4171e73a62c052563aee5355084a7c5930034ee
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-06-14 14:35:28 +0300

    Рефакторинг батона

--- packages/ui/components/src/Button/Button.jsx
+++ packages/ui/components/src/Button/Button.jsx
@@ -1,90 +1,15 @@
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { withStyles } from '@astral-frontend/styles';
+import { makeStyles } from '@astral-frontend/styles';
 
 import ButtonBase from '../ButtonBase';
 import CircularProgress from '../CircularProgress';
 
-const getIsBlockVariant = variant => (variant === 'text' || variant === 'textBlock');
-const getIsTextVariant = variant => (variant === 'textBlock' || variant === 'regularBlock');
+const getIsBlockVariant = variant => variant === 'text' || variant === 'textBlock';
+const getIsTextVariant = variant => variant === 'textBlock' || variant === 'regularBlock';
 
-const Button = ({
-  loading,
-  disabled,
-  classes,
-  className: classNameProp,
-  variant,
-  color,
-  size,
-  children,
-  ...props
-}) => {
-  const isTextVariant = getIsBlockVariant(variant);
-  const isBlockVariant = getIsTextVariant(variant);
-
-  const className = cn(
-    classes.root,
-    {
-      [classes.disabledText]: disabled && isTextVariant,
-      [classes.disabledBlock]: disabled && !isTextVariant,
-      [classes.loading]: loading,
-      [classes.text]: variant === 'text',
-      [classes.textBlock]: variant === 'textBlock',
-      [classes.regular]: variant === 'regular',
-      [classes.regularBlock]: variant === 'regularBlock',
-      [classes.primaryMainBackground]:
-        color === 'primary' && !isTextVariant,
-      [classes.small]: size === 'small' && !isBlockVariant,
-      [classes.medium]: size === 'medium' && !isBlockVariant,
-      [classes.large]: size === 'large' && !isBlockVariant,
-      [classes.smallBlock]: size === 'small' && isBlockVariant,
-      [classes.mediumBlock]: size === 'medium' && isBlockVariant,
-      [classes.largeBlock]: size === 'large' && isBlockVariant,
-    },
-    classNameProp,
-  );
-
-  return (
-    <div className={classes.wrapper}>
-      <ButtonBase disabled={disabled || loading} className={className} {...props}>
-        {children}
-      </ButtonBase>
-      {loading ? <CircularProgress className={classes.loader} size={20} /> : null}
-    </div>
-  );
-};
-
-Button.defaultProps = {
-  className: null,
-  disabled: false,
-  loading: false,
-  color: 'primary',
-  variant: 'text',
-  size: 'medium',
-};
-
-Button.propTypes = {
-  disabled: PropTypes.bool,
-  loading: PropTypes.bool,
-  classes: PropTypes.shape().isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired,
-  /**
-   * Цвет
-   */
-  color: PropTypes.oneOf(['primary', 'secondary']),
-  /**
-   * Вариант использования
-   */
-  variant: PropTypes.oneOf(['text', 'textBlock', 'regular', 'regularBlock']),
-  /**
-   * Размер
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
-
-export default withStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     padding: '0 20px',
     fontSize: '14px',
@@ -107,9 +32,7 @@ export default withStyles(theme => ({
   regular: {
     borderRadius: '4px',
   },
-  regularBlock: {
-
-  },
+  regularBlock: {},
   small: {
     minHeight: '32px',
   },
@@ -136,16 +59,7 @@ export default withStyles(theme => ({
       backgroundColor: theme.palette.primary.dark,
     },
   },
-  wrapper: {
-    display: 'inline-flex',
-    position: 'relative',
-  },
   loader: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: '-10px',
-    marginLeft: '-10px',
     color: theme.palette.primary.main,
   },
   disabledText: {
@@ -159,4 +73,75 @@ export default withStyles(theme => ({
     color: 'transparent',
     backgroundColor: theme.palette.grey[100],
   },
-}))(Button);
+}));
+
+const Button = ({
+  loading,
+  disabled,
+  className: classNameProp,
+  variant,
+  color,
+  size,
+  children,
+  ...props
+}) => {
+  const classes = useStyles();
+  const isTextVariant = getIsBlockVariant(variant);
+  const isBlockVariant = getIsTextVariant(variant);
+  const className = cn(
+    classes.root,
+    {
+      [classes.disabledText]: disabled && isTextVariant,
+      [classes.disabledBlock]: disabled && !isTextVariant,
+      [classes.loading]: loading,
+      [classes.text]: variant === 'text',
+      [classes.textBlock]: variant === 'textBlock',
+      [classes.regular]: variant === 'regular',
+      [classes.regularBlock]: variant === 'regularBlock',
+      [classes.primaryMainBackground]: color === 'primary' && !isTextVariant,
+      [classes.small]: size === 'small' && !isBlockVariant,
+      [classes.medium]: size === 'medium' && !isBlockVariant,
+      [classes.large]: size === 'large' && !isBlockVariant,
+      [classes.smallBlock]: size === 'small' && isBlockVariant,
+      [classes.mediumBlock]: size === 'medium' && isBlockVariant,
+      [classes.largeBlock]: size === 'large' && isBlockVariant,
+    },
+    classNameProp,
+  );
+
+  return (
+    <ButtonBase disabled={disabled || loading} className={className} {...props}>
+      {loading ? <CircularProgress className={classes.loader} size={20} /> : children}
+    </ButtonBase>
+  );
+};
+
+Button.defaultProps = {
+  className: null,
+  disabled: false,
+  loading: false,
+  color: 'primary',
+  variant: 'text',
+  size: 'medium',
+};
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  /**
+   * Цвет
+   */
+  color: PropTypes.oneOf(['primary', 'secondary']),
+  /**
+   * Вариант использования
+   */
+  variant: PropTypes.oneOf(['text', 'textBlock', 'regular', 'regularBlock']),
+  /**
+   * Размер
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+export default Button;
