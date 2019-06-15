52bc60544301933a0d8a8eccb4a49ab6712c66d7
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-06-15 14:11:01 +0300

    add jest config

--- packages/ui/validations/src/mustBeOGRN/mustBeOGRN.test.js
+++ packages/ui/validations/src/mustBeOGRN/mustBeOGRN.test.js
@@ -1,13 +1,7 @@
 import mustBeOGRN from './mustBeOGRN';
 
-describe('`mustBeOGRN` validation rule', () => {
-  test('should return an error object if value is invalid', () => {
-    expect(mustBeOGRN(2222222222)).toEqual('Неверный ОГРН. Введите корректный ОГРН.');
-    expect(mustBeOGRN(222222222222222)).toEqual('Неверный ОГРН. Введите корректный ОГРН.');
-  });
-
-  test('should return empty object if data is valid', () => {
-    expect(mustBeOGRN(5024282499324)).toEqual(null);
-    expect(mustBeOGRN(1118453466680)).toEqual(null);
+describe('mustBeOGRN', () => {
+  it('должна возвращать null если value является валидным ОГРН', () => {
+    expect(mustBeOGRN('1375748871189')).toEqual(null);
   });
 });
--- packages/ui/validations/src/mustBeSNILS/mustBeSNILS.js
+++ packages/ui/validations/src/mustBeSNILS/mustBeSNILS.js
@@ -1,43 +1,14 @@
-const mustBeSNILS = () => null;
-// if (!/^(\d{11})$/.test(value)) {
-//   return 'Неверный СНИЛС. Введите корректный СНИЛС.';
-// }
+/**
+ * Функция валидации СНИЛС
+ *
+ * @param {string} value - Валидируемое значение
+ */
+const mustBeSNILS = (value) => {
+  if (!/^(\d{11})$/.test(value)) {
+    return 'Неверный СНИЛС. Введите корректный СНИЛС.';
+  }
 
-// const snils = value
-//   .toString()
-//   .split('')
-//   .map(x => Number(x));
-
-// if (snils.length !== 11) {
-//   return 'Неверный СНИЛС. Введите корректный СНИЛС.';
-// }
-
-// const b = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0];
-// const checkSum = snils.reduce(
-//   (sum, digit, i) => sum + (digit * (b[i])),
-//   0,
-// );
-
-// let checkDigit = 0;
-// if (checkSum < 100) {
-//   checkDigit = checkSum;
-// }
-// if (checkSum === 100) {
-//   checkDigit = 0;
-// }
-// if (checkSum > 100) {
-//   checkDigit = Number(checkSum % 101);
-//   if (checkDigit === 100) {
-//     checkDigit = 0;
-//   } else {
-//     checkDigit = checkDigit;
-//   }
-// }
-
-// const c = snils.slice(-2).join('');
-
-// if (checkDigit == c) {
-//   return null;
-// }
+  return null;
+};
 
 export default mustBeSNILS;
