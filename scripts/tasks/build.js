b251c65aaafa366fa9773c201fb0aa04b5df81ab
Andrey Potyomkin <ipad_nw@icloud.com>
2019-06-18 12:28:26 +0300

    Поправлен npmrc для релизов

--- scripts/tasks/build.js
+++ scripts/tasks/build.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 
 module.exports = () => {
-  const { code } = shell.exec('babel ./src --out-dir ./lib --ignore "**/*.percy.jsx"');
+  const { code } = shell.exec('printenv && babel ./src --out-dir ./lib --ignore "**/*.percy.jsx"');
 
   if (code !== 0) {
     shell.exit(code);
