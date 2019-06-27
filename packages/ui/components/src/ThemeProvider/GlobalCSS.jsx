a794d2228a93c3b85d6c3000c583b147efe34ae3
albert <merkel.albert1997@gmail.com>
2019-06-27 16:26:02 +0300

    try to fix

--- packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
+++ packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
@@ -3,30 +3,34 @@ import { withStyles } from '@astral-frontend/styles';
 const GlobalCSS = () => null;
 
 export default withStyles(
-  theme => ({
-    '@global': {
-      html: {
-        WebkitFontSmoothing: 'antialiased',
-        MozOsxFontSmoothing: 'grayscale',
-        boxSizing: 'border-box',
-      },
-      'html, body, #root': {
-        width: '100vw',
-        height: '100vh',
-        margin: 0,
-        padding: 0,
-      },
-      body: {
-        backgroundColor: theme.palette.background.default,
-        ...theme.typography.body2,
-        '@media print': {
-          backgroundColor: theme.palette.common.white,
+  (theme) => {
+    console.log(theme);
+
+    return {
+      '@global': {
+        html: {
+          WebkitFontSmoothing: 'antialiased',
+          MozOsxFontSmoothing: 'grayscale',
+          boxSizing: 'border-box',
+        },
+        'html, body, #root': {
+          width: '100vw',
+          height: '100vh',
+          margin: 0,
+          padding: 0,
+        },
+        body: {
+          backgroundColor: theme.palette.background.default,
+          ...theme.typography.body2,
+          '@media print': {
+            backgroundColor: theme.palette.common.white,
+          },
+        },
+        '*, *::before, *::after': {
+          boxSizing: 'inherit',
         },
       },
-      '*, *::before, *::after': {
-        boxSizing: 'inherit',
-      },
-    },
-  }),
+    };
+  },
   { name: 'GlobalCSS' },
 )(GlobalCSS);
