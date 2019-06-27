341c9a8a51c7c899c82fbc6f5f5f0694a7abb5b2
albert <merkel.albert1997@gmail.com>
2019-06-27 17:00:46 +0300

    revert

--- packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
+++ packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
@@ -3,34 +3,30 @@ import { withStyles } from '@astral-frontend/styles';
 const GlobalCSS = () => null;
 
 export default withStyles(
-  (theme) => {
-    console.log(theme);
-
-    return {
-      '@global': {
-        html: {
-          WebkitFontSmoothing: 'antialiased',
-          MozOsxFontSmoothing: 'grayscale',
-          boxSizing: 'border-box',
-        },
-        'html, body, #root': {
-          width: '100vw',
-          height: '100vh',
-          margin: 0,
-          padding: 0,
-        },
-        body: {
-          backgroundColor: theme.palette.background.default,
-          ...theme.typography.body2,
-          '@media print': {
-            backgroundColor: theme.palette.common.white,
-          },
-        },
-        '*, *::before, *::after': {
-          boxSizing: 'inherit',
+  theme => ({
+    '@global': {
+      html: {
+        WebkitFontSmoothing: 'antialiased',
+        MozOsxFontSmoothing: 'grayscale',
+        boxSizing: 'border-box',
+      },
+      'html, body, #root': {
+        width: '100vw',
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+      },
+      body: {
+        backgroundColor: theme.palette.background.default,
+        ...theme.typography.body2,
+        '@media print': {
+          backgroundColor: theme.palette.common.white,
         },
       },
-    };
-  },
+      '*, *::before, *::after': {
+        boxSizing: 'inherit',
+      },
+    },
+  }),
   { name: 'GlobalCSS' },
 )(GlobalCSS);
