52e40d5a0bf3c98345c327742f57d592ca2570aa
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-07-11 13:36:26 +0300

    fix

--- packages/ui/components/src/TableCell/TableCell.jsx
+++ packages/ui/components/src/TableCell/TableCell.jsx
@@ -4,15 +4,15 @@ import { TableCell as MuiTableCell } from '@astral-frontend/core';
 import { makeStyles } from '@astral-frontend/styles';
 
 const useStyles = makeStyles(
-  () => ({
+  theme => ({
     root: {
       // FIXME
       color: 'rgba(29, 63, 102, 0.62)',
     },
     head: {
-      padding: '15px',
+      padding: '20px',
       fontSize: '12px',
-      fontWeight: 'bold',
+      fontWeight: theme.typography.fontWeightBold,
     },
   }),
   { name: 'TableCell' },
