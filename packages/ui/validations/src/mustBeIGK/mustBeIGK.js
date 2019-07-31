3daa29203ad5f15010448a2200ac5d88b5ae7341
albert <merkel.albert1997@gmail.com>
2019-07-31 11:46:39 +0300

    Идентификатор гос контракта

---
+++ packages/ui/validations/src/mustBeIGK/mustBeIGK.js
@@ -0,0 +1,9 @@
+const mustBeINN = (value) => {
+  if (!/^(\d{20,25})$/.test(value)) {
+    return 'Неверный идентификатор гос. контракта. Введите корректный ИГК.';
+  }
+
+  return null;
+};
+
+export default mustBeINN;
