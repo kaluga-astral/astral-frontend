ee38b43b8a969ec3c4cda7b636342dd221dd83a6
Vladimir Prokhorov <9wowa9@gmail.com>
2019-08-29 14:26:54 +0300

    Исправляет валидации ИНН для ЮЛ/ИП

--- packages/ui/validations/src/mustBeIPINN/mustBeIPINN.test.js
+++ packages/ui/validations/src/mustBeIPINN/mustBeIPINN.test.js
@@ -1,20 +1,21 @@
 import mustBeIPINN from './mustBeIPINN';
 
 describe('mustBeIPINN', () => {
-  it('должна возвращать сообщение об ошибке если value < 10 символов', () => {
-    const value = new Array(9).fill('1').join('');
-
-    expect(mustBeIPINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
-  });
-  it('должна возвращать сообщение об ошибке если value > 10 символов и value < 12 символов', () => {
+  it('должна возвращать сообщение об ошибке если value < 12 символов', () => {
     const value = new Array(11).fill('1').join('');
 
     expect(mustBeIPINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
   });
-  it('должна возвращать сообщение об ошибке если value > 12 символов', () => {
+  it('должна возвращать сообщение об ошибке если value > 12 ', () => {
     const value = new Array(13).fill('1').join('');
 
     expect(mustBeIPINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
+  });
+  it('должна возвращать null если value === 12 символов (контрольная сумма верна)', () => {
+    expect(mustBeIPINN('683000788049')).toEqual(null);
+  });
+  it('должна возвращать null если value === 12 символов (контрольная сумма неверна)', () => {
+    expect(mustBeIPINN('683000700049')).toEqual('Неверный ИНН. Введите корректный ИНН.');
   });
   it('должна возвращать null если value является валидным ИНН ИП', () => {
     expect(mustBeIPINN('683000788049')).toEqual(null);
--- packages/ui/validations/src/mustBeULINN/mustBeULINN.test.js
+++ packages/ui/validations/src/mustBeULINN/mustBeULINN.test.js
@@ -6,15 +6,16 @@ describe('mustBeULINN', () => {
 
     expect(mustBeULINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
   });
-  it('должна возвращать сообщение об ошибке если value > 10 символов и value < 12 символов', () => {
+  it('должна возвращать сообщение об ошибке если value > 10', () => {
     const value = new Array(11).fill('1').join('');
 
     expect(mustBeULINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
   });
-  it('должна возвращать сообщение об ошибке если value > 12 символов', () => {
-    const value = new Array(13).fill('1').join('');
-
-    expect(mustBeULINN(value)).toEqual('Неверный ИНН. Введите корректный ИНН.');
+  it('должна возвращать null если value === 10 символов (контрольная сумма верна)', () => {
+    expect(mustBeULINN('3703020990')).toEqual(null);
+  });
+  it('должна возвращать null если value === 10 символов (контрольная сумма неверна)', () => {
+    expect(mustBeULINN('3703020000')).toEqual('Неверный ИНН. Введите корректный ИНН.');
   });
   it('должна возвращать null если value является валидным ИНН ЮЛ', () => {
     expect(mustBeULINN('3703020990')).toEqual(null);
