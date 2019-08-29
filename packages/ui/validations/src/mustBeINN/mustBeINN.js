00a169564bac2d728a5d51b21264d13c72a584f0
Vladimir Prokhorov <9wowa9@gmail.com>
2019-08-29 14:54:44 +0300

    Рефакторинг валидаций ИНН

--- packages/ui/validations/src/mustBeINN/mustBeINN.js
+++ packages/ui/validations/src/mustBeINN/mustBeINN.js
@@ -1,36 +1,12 @@
-const calcCheckNum = (digitsOfvalue, weights) => {
-  const checkSum = digitsOfvalue.reduce((sum, digit, i) => sum + digit * weights[i], 0);
-  const checkNum = checkSum % 11;
-
-  return checkNum > 9 ? checkNum % 10 : checkNum;
-};
+import mustBeIPINN from '../mustBeIPINN';
+import mustBeULINN from '../mustBeULINN';
 
 /**
  * Проверка валидации ИНН на корректность
  * @param {ИНН} value
  */
 const mustBeINN = (value) => {
-  if (!/^(\d{10})$/.test(value) && !/^(\d{12})$/.test(value)) {
-    return 'Неверный ИНН. Введите корректный ИНН.';
-  }
-
-  const digitsOfvalue = value
-    .toString()
-    .split('')
-    .map(x => Number(x));
-
-  if (
-    digitsOfvalue.length === 10
-    && calcCheckNum(digitsOfvalue, [2, 4, 10, 3, 5, 9, 4, 6, 8, 0]) !== digitsOfvalue[9]
-  ) {
-    return 'Неверный ИНН. Введите корректный ИНН.';
-  }
-
-  if (
-    digitsOfvalue.length === 12
-    && (calcCheckNum(digitsOfvalue, [7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0, 0]) !== digitsOfvalue[10]
-      || calcCheckNum(digitsOfvalue, [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0]) !== digitsOfvalue[11])
-  ) {
+  if (mustBeULINN(value) && mustBeIPINN(value)) {
     return 'Неверный ИНН. Введите корректный ИНН.';
   }
 
