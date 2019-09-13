3a3bd3e3b4bd1fcf07de6928ee39ae4b9b3ad54c
Vladimir Prokhorov <9wowa9@gmail.com>
2019-09-13 16:29:21 +0300

    Добавляет константы в index

--- packages/ui/constants/src/index.js
+++ packages/ui/constants/src/index.js
@@ -1,5 +1,8 @@
 import * as httpStatuses from './httpStatuses';
 import * as time from './time';
 import * as organizationTypes from './organizationTypes';
+import * as organizationTypesValidations from './organizationTypesValidations';
 
-export { httpStatuses, time, organizationTypes };
+export {
+  httpStatuses, time, organizationTypes, organizationTypesValidations,
+};
