e7a9cba331bc7843e2cf5a1b8a1fe1b516049147
albert <merkel.albert1997@gmail.com>
2019-10-30 20:15:35 +0300

    commonjs babel plugin

--- packages/tools/babel-preset-react-app/index.js
+++ packages/tools/babel-preset-react-app/index.js
@@ -16,5 +16,6 @@ module.exports = () => ({
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-modules-commonjs',
   ],
 });
