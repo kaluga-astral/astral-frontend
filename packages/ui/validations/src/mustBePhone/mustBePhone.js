6cc3b1a9a168bd039e23d6447c669bd768a393f1
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-12-05 10:48:03 +0300

    Валидация телефона

--- packages/ui/validations/src/mustBePhone/mustBePhone.js
+++ packages/ui/validations/src/mustBePhone/mustBePhone.js
@@ -1,9 +1,10 @@
-const phoneRegExp = /^\+?7\d{10}$/;
+const phoneRegExp = /^\+?79(\d{9})$/;
 
 function mustBePhone(value) {
   if (phoneRegExp.test(value)) {
     return null;
   }
+
   return 'Номер телефона имеет некорректный формат';
 }
 export default mustBePhone;
