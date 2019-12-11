6fd9b0134cef0e47bb421ba2c2d51a43ece2dfcd
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-12-11 13:38:40 +0300

    fix

--- packages/widgets/data-list/src/index.js
+++ packages/widgets/data-list/src/index.js
@@ -1,3 +1,4 @@
 export { default as DataList } from './DataList';
+export { default as DataListEmptyState } from './DataListEmptyState';
 export { default as DataListItem } from './DataListItem';
 export { default as DataListRowActions } from './DataListRowActions';
