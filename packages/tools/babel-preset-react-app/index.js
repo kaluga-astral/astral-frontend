0b018069051fad202474ec1b83780ebd8c339349
Dima Denisov <2denisov@bk.ru>
2019-12-19 12:12:26 +0300

    babel config

--- packages/tools/babel-preset-react-app/index.js
+++ packages/tools/babel-preset-react-app/index.js
@@ -15,6 +15,16 @@ module.exports = () => ({
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-syntax-dynamic-import',
   ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react',
+      ],
+    },
+  },
 });
