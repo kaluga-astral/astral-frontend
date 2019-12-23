244b43497ff3dd469c6ab52da351d4b273854008
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-12-23 12:26:11 +0300

    Добавляет DetailsListItem Изменяет paddingTop/paddingBottom у SlideModalContent

---
+++ packages/ui/components/src/DetailsListItem/DetailsListItem.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { makeStyles } from '@astral-frontend/styles';
+
+import ListItem from '../ListItem';
+
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      display: 'flex',
+      fontSize: theme.typography.pxToRem(14),
+    },
+    title: {
+      color: theme.palette.gray[600],
+      marginRight: theme.spacing(1),
+    },
+    value: {
+      display: 'inline-block',
+      color: theme.palette.gray[800],
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+    },
+  }),
+  { name: 'DetailsListItem' },
+);
+
+const DetailsListItem = ({ title, value }) => {
+  const classes = useStyles();
+
+  return (
+    <ListItem className={classes.root} dense disableGutters>
+      <span className={classes.title}>{title || 'Не указано'}</span>
+      <span className={classes.value}>{value || 'Не указано'}</span>
+    </ListItem>
+  );
+};
+
+DetailsListItem.defaultProps = {
+  value: null,
+};
+
+DetailsListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
+
+export default DetailsListItem;
