6c91322a432b7168b19599010031b823c5f8dea2
Dima Denisov <2denisov@bk.ru>
2019-12-25 10:22:25 +0300

    remove MainModal from project

--- packages/ui/dashboard-layout/src/MainModal/index.js
+++
@@ -1,9 +0,0 @@
-import { SlideModal } from '@astral-frontend/components';
-
-import MainModal from './MainModal';
-
-MainModal.Title = SlideModal.Title;
-MainModal.Content = SlideModal.Content;
-MainModal.Footer = SlideModal.Footer;
-
-export default MainModal;
