c30829d5be1957ea8b114b44d15d625454798f59
Andrey Potyomkin <ipad_nw@icloud.com>
2020-02-14 16:25:41 +0300

    в oidc-provider поправлена функция для определения html метода (появились websocket)

--- packages/oidc-provider/src/utils/httpMethods/httpMethods.js
+++ packages/oidc-provider/src/utils/httpMethods/httpMethods.js
@@ -1,6 +1,6 @@
 const isOptionsMethod = req => req.method === 'OPTIONS';
 const isGetMethod = req => req.method === 'GET';
-const isHtmlRequest = req => req.headers.accept.includes('text/html');
+const isHtmlRequest = req => (req.headers.accept || '').includes('text/html');
 const isFirstHtmlRequest = req => isGetMethod(req) && isHtmlRequest(req);
 
 module.exports = {
