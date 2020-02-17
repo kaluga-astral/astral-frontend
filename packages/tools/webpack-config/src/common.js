201c620a387c9c6206acea04070893ab657b33f0
Pavel Perevezentsev <perevezenzev@gmail.com>
2020-02-17 11:19:50 +0300

    fix

--- packages/tools/webpack-config/src/common.js
+++ packages/tools/webpack-config/src/common.js
@@ -50,7 +50,6 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
-          'thread-loader',
           {
             loader: 'url-loader',
             options: {
