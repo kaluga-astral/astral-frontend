552a91e43046ab669ff56d034314cf127d55b025
Dima Denisov <2denisov@bk.ru>
2020-04-01 18:23:35 +0300

    rewrite Month with @astral-frontend/components

--- packages/ui/components/src/DateRangePicker/components/Month.jsx
+++ packages/ui/components/src/DateRangePicker/components/Month.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Paper, Grid, Typography, ThemeProvider } from '@astral-frontend/core';
+import { Grid, Typography } from '@astral-frontend/core';
 import { makeStyles } from '@astral-frontend/styles';
 import {
   getDate,
@@ -9,6 +9,8 @@ import {
   format,
   isWithinInterval,
 } from 'date-fns';
+import FlexContainer from '../../FlexContainer';
+import FlexItem from '../../FlexItem';
 import {
   chunks,
   getDaysInMonth,
@@ -61,84 +63,83 @@ const Month = ({
   const [back, forward] = navState;
 
   return (
-    <Paper square elevation={0} className={classes.root}>
-      <Grid container>
-        <Header
-          date={date}
-          setDate={setDate}
-          nextDisabled={!forward}
-          prevDisabled={!back}
-          onClickPrevious={() => handlers.onMonthNavigate(marker, -1)}
-          onClickNext={() => handlers.onMonthNavigate(marker, 1)}
-          months={months}
-        />
+    <FlexContainer direction="column" className={classes.root}>
+      <FlexItem
+        component={Header}
+        date={date}
+        setDate={setDate}
+        nextDisabled={!forward}
+        prevDisabled={!back}
+        onClickPrevious={() => handlers.onMonthNavigate(marker, -1)}
+        onClickNext={() => handlers.onMonthNavigate(marker, 1)}
+        months={months}
+      />
 
-        <Grid
-          item
-          container
-          direction="row"
-          justify="center"
-          className={classes.weekDaysContainer}
-        >
-          {weekDays.map(day => (
-            <Typography
-              align="center"
-              key={day}
-              variant="subtitle2"
-              className={classes.weekDay}
+      <FlexItem
+        component={FlexContainer}
+        direction="row"
+        justifyContent="center"
+        className={classes.weekDaysContainer}
+      >
+        {weekDays.map(day => (
+          <FlexItem
+            component={Typography}
+            align="center"
+            key={day}
+            variant="subtitle2"
+            className={classes.weekDay}
+          >
+            {day}
+          </FlexItem>
+        ))}
+      </FlexItem>
+
+      <FlexItem
+        component={FlexContainer}
+        direction="column"
+        justify="space-between"
+        className={classes.daysContainer}
+      >
+        {chunks(getDaysInMonth(date, { weekStartsOn: 1 }), 7).map(
+          (week, idx) => (
+            <FlexItem
+              component={FlexContainer}
+              key={idx}
+              direction="row"
+              justifyContent="center"
+              className={classes.week}
             >
-              {day}
-            </Typography>
-          ))}
-        </Grid>
+              {week.map(day => {
+                const isStart = isStartOfRange(dateRange, day);
+                const isEnd = isEndOfRange(dateRange, day);
+                const isRangeOneDay = isRangeSameDay(dateRange);
+                const highlighted =
+                  inDateRange(dateRange, day) || helpers.inHoverRange(day);
 
-        <Grid
-          item
-          container
-          direction="column"
-          justify="space-between"
-          className={classes.daysContainer}
-        >
-          {chunks(getDaysInMonth(date, { weekStartsOn: 1 }), 7).map(
-            (week, idx) => (
-              <Grid
-                key={idx}
-                container
-                direction="row"
-                justify="center"
-                className={classes.week}
-              >
-                {week.map(day => {
-                  const isStart = isStartOfRange(dateRange, day);
-                  const isEnd = isEndOfRange(dateRange, day);
-                  const isRangeOneDay = isRangeSameDay(dateRange);
-                  const highlighted =
-                    inDateRange(dateRange, day) || helpers.inHoverRange(day);
-
-                  return (
-                    <Day
-                      key={format(day, 'MM-dd-yyyy')}
-                      filled={isStart || isEnd}
-                      outlined={isToday(day)}
-                      highlighted={highlighted && !isRangeOneDay}
-                      disabled={
-                        !isSameMonth(date, day) ||
-                        !isWithinInterval(day, { start: minDate, end: maxDate })
-                      }
-                      startOfRange={isStart && !isRangeOneDay}
-                      endOfRange={isEnd && !isRangeOneDay}
-                      onClick={() => handlers.onDayClick(day)}
-                      onHover={() => handlers.onDayHover(day)}
-                      value={getDate(day)}
-                    />
-                  );
-                })}
-              </Grid>
-            ),
-          )}
-        </Grid>
-      </Grid>
-    </Paper>
+                return (
+                  <FlexItem
+                    component={Day}
+                    key={format(day, 'MM-dd-yyyy')}
+                    filled={isStart || isEnd}
+                    outlined={isToday(day)}
+                    highlighted={highlighted && !isRangeOneDay}
+                    disabled={
+                      !isSameMonth(date, day) ||
+                      !isWithinInterval(day, { start: minDate, end: maxDate })
+                    }
+                    startOfRange={isStart && !isRangeOneDay}
+                    endOfRange={isEnd && !isRangeOneDay}
+                    onClick={() => handlers.onDayClick(day)}
+                    onHover={() => handlers.onDayHover(day)}
+                    value={getDate(day)}
+                  />
+                );
+              })}
+            </FlexItem>
+          ),
+        )}
+      </FlexItem>
+    </FlexContainer>
   );
 };
 
