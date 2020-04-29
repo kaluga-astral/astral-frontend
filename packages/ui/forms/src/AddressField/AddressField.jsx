beabc4c1abf10a0a0827d68203eedcdc8a54f65a
Pavel Perevezentsev <perevezenzev@gmail.com>
2020-04-29 17:09:38 +0300

    Исправления

--- packages/ui/forms/src/AddressField/AddressField.jsx
+++ packages/ui/forms/src/AddressField/AddressField.jsx
@@ -6,8 +6,6 @@ import DaDataContext from '../DaDataContext/DaDataContext';
 const FormAddressField = props => {
   const { fetchAddressSuggestions } = React.useContext(DaDataContext);
 
-  console.log('FormAddressField', props.value);
-
   return (
     <AsyncAutocompleteField {...props} fetchOptions={fetchAddressSuggestions} />
   );
