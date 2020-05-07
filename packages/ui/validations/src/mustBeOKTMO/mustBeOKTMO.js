e5714643ea69ce43facd1779d711e636085e8f18
Владимир Прохоров <vladimirprohorov@MacBook-Pro.local>
2020-05-07 16:20:44 +0300

    Fix naming

--- packages/ui/validations/src/mustBeOKTMO/mustBeOKTMO.js
+++ packages/ui/validations/src/mustBeOKTMO/mustBeOKTMO.js
@@ -1,6 +1,6 @@
 const mustBeOKPO = value => {
   if (!/^(\d{8,11})$/.test(value)) {
-    return 'Неверный ОКПО. Введите корректный ОКПО.';
+    return 'Неверный ОКТМО. Введите корректный ОКТМО.';
   }
 
   return null;
