825f27c64f43b45610ae891121ed1c5697c51fa5
Pavel Perevezentsev <perevezenzev@gmail.com>
2020-07-21 15:42:16 +0300

    Испрвления и доработки в TransferTemplatedDataList

--- packages/widgets/transfer-templated-data-lis/src/TransferTemplatedDataList/TransferTemplatedDataList.jsx
+++ packages/widgets/transfer-templated-data-lis/src/TransferTemplatedDataList/TransferTemplatedDataList.jsx
@@ -1,11 +1,18 @@
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { List, Typography, Box } from '@astral-frontend/components';
+import {
+  List,
+  Typography,
+  Box,
+  IconButton,
+  SvgIcon,
+} from '@astral-frontend/components';
 
 import { makeStyles } from '@astral-frontend/styles';
-
 import DataList from '@astral-frontend/data-list';
+
+import { TransferTemplatedDataListSection } from './TransferTemplatedDataListSection';
 
 const useStyles = makeStyles(
   theme => ({
@@ -13,7 +20,6 @@ const useStyles = makeStyles(
       flexGrow: 1,
       overflowY: 'auto',
     },
-    list: {},
     counter: {
       height: '21px',
       minWidth: '32px',
@@ -23,6 +29,16 @@ const useStyles = makeStyles(
       textAlign: 'center',
       borderRadius: `${theme.shape.borderRadius}px`,
       backgroundColor: theme.palette.primary.light,
+    },
+    list: {},
+    listItem: {
+      '&:hover $rowActions': {
+        opacity: 1,
+      },
+    },
+    rowActions: {
+      opacity: 0,
+      transition: theme.transitions.create(['opacity']),
     },
   }),
   { name: 'TransferTemplatedDataList' },
@@ -46,7 +62,30 @@ const TransferTemplatedDataList = ({
     ),
     [],
   );
-  const renderItem = React.useCallback(
+  const renderDestinationListItem = React.useCallback(
+    ({ data, key }) => {
+      const handleRemoveIconButtonClick = () => {
+        setSelectedItems(prevValue => {
+          return prevValue.filter(({ id }) => id !== data.id);
+        });
+      };
+
+      return (
+        <ListItemComponent key={key} className={classes.listItem} data={data}>
+          <Box className={classes.rowActions} display="flex">
+            <IconButton onClick={handleRemoveIconButtonClick}>
+              <SvgIcon>
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+              </SvgIcon>
+            </IconButton>
+          </Box>
+        </ListItemComponent>
+      );
+    },
+    [queryResult.loading],
+  );
+  const renderSourceListItem = React.useCallback(
     ({ data, key }) => (
       <ListItemComponent
         key={key}
@@ -58,56 +97,45 @@ const TransferTemplatedDataList = ({
     [queryResult.loading],
   );
 
-  React.useEffect(() => {
-    if (queryResult.data.items) {
-      setTimeout(() => {
-        setSelectedItems([{ id: 'test-id', value: 'value' }]);
-      }, 1500);
-    }
-  }, [queryResult.loading]);
-
   return (
     <div className={classes.root}>
-      <Box display="flex">
-        <Typography gutterBottom color="primary">
-          Отсканированные
-        </Typography>
-        <Box className={classes.counter}>
-          {queryResult.loading ? null : String(selectedItems.length)}
-        </Box>
-      </Box>
-      <DataList
-        pageSize={15}
-        awaitMore={false}
-        queryResult={{
-          called: true,
-          loading: false,
-          error: null,
-          data: {
-            items: selectedItems,
-          },
-        }}
-        listRenderer={listRenderer}
-        renderItem={renderItem}
-      />
-      <Box display="flex" mt={4}>
-        <Typography gutterBottom color="primary">
-          Код маркировки
-        </Typography>
-        <Box className={classes.counter}>
-          {queryResult.loading
+      <TransferTemplatedDataListSection
+        mb={4}
+        title="Отсканированные"
+        count={queryResult.loading ? null : String(selectedItems.length)}
+      >
+        <DataList
+          pageSize={15}
+          awaitMore={false}
+          queryResult={{
+            called: true,
+            loading: false,
+            error: null,
+            data: {
+              items: selectedItems,
+            },
+          }}
+          listRenderer={listRenderer}
+          renderItem={renderDestinationListItem}
+        />
+      </TransferTemplatedDataListSection>
+      <TransferTemplatedDataListSection
+        title="Код маркировки"
+        count={
+          queryResult.loading
             ? null
-            : String(queryResult.data.totalCount - selectedItems.length)}
-        </Box>
-      </Box>
-      <DataList
-        {...props}
-        selectedItems={selectedItems}
-        setSelectedItems={setSelectedItems}
-        queryResult={queryResult}
-        listRenderer={listRenderer}
-        renderItem={renderItem}
-      />
+            : String(queryResult.data.totalCount - selectedItems.length)
+        }
+      >
+        <DataList
+          {...props}
+          selectedItems={selectedItems}
+          setSelectedItems={setSelectedItems}
+          queryResult={queryResult}
+          listRenderer={listRenderer}
+          renderItem={renderSourceListItem}
+        />
+      </TransferTemplatedDataListSection>
     </div>
   );
 };
