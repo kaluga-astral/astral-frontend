1c9ab87b5c5cb178ce95a6edfe6a4d0805c68a55
Andrey Potyomkin <ipad_nw@icloud.com>
2020-08-28 12:45:31 +0300

    Auth-server. Убран из логов auth токен

--- packages/oidc-auth-server/src/middlewares/httpLogger.js
+++ packages/oidc-auth-server/src/middlewares/httpLogger.js
@@ -1,8 +1,22 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
+const headersLogFilter = ({ headers }) => {
+  const { Authorization, ...logHeaders } = headers;
+
+  return logHeaders;
+};
+
+const reqLogFilter = (req, propName) => {
+  if (propName === 'headers') return headersLogFilter(req);
+
+  return req[propName];
+};
+
 module.exports = expressWinston.logger({
   transports: [new winston.transports.Console()],
+  requestFilter: reqLogFilter,
+  expressFormat: true,
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.colorize(),
