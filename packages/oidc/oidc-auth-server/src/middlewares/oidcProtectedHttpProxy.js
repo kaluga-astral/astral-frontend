d6b571e4e3155e087764a9036dee7c9d68a5fdd1
Andrey Potyomkin <ipad_nw@icloud.com>
2020-08-31 16:04:26 +0300

    Auth-server. Нормализация кук сделана для всех роутов, назависимо защищенные они или нет

--- packages/oidc/oidc-auth-server/src/middlewares/oidcProtectedHttpProxy.js
+++ packages/oidc/oidc-auth-server/src/middlewares/oidcProtectedHttpProxy.js
@@ -1,10 +1,8 @@
 const { compose } = require('compose-middleware');
 
 const createHttpProxy = require('./httpProxy');
-const normalizeCookie = require('./normalizeCookie');
 
-// normalizeCookie здесь необходим для того, чтобы производить перезапись невалидный значений cookie
 const createOidcProtectedHttpProxy = oidcProtected => proxyParams =>
-  compose([oidcProtected, normalizeCookie, createHttpProxy(proxyParams)]);
+  compose([oidcProtected, createHttpProxy(proxyParams)]);
 
 module.exports = createOidcProtectedHttpProxy;
