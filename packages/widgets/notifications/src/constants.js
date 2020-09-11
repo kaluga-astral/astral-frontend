c7a68f483950f1d1a33792967bb5cc3bb3ec83bc
Dima Denisov <2denisov@bk.ru>
2020-09-11 14:59:10 +0300

    add notification types constant

---
+++ packages/widgets/notifications/src/constants.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/prefer-default-export
+export const NOTIFICATIONS_TYPES = {
+  custom: {
+    id: 'custom',
+  },
+  system: {
+    id: 'system',
+  },
+  success: {
+    id: 'success',
+  },
+  successTimer: {
+    id: 'successTimer',
+  },
+  error: {
+    id: 'error',
+  },
+  errorTimer: {
+    id: 'errorTimer',
+  },
+  info: {
+    id: 'info',
+  },
+  infoTimer: {
+    id: 'infoTimer',
+  },
+  delete: {
+    id: 'delete',
+  },
+};
