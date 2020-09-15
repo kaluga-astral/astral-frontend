f82919c0b42a8f19853cd69c4b08522f8d36cb71
Vladimir <9wowa9@gmail.com>
2020-09-15 12:49:00 +0300

    Добавляет элемент CssBaseline. Глобальное изменение scroollbar для astralEDOTheme.

--- packages/ui/themes/src/AstralEDOTheme.js
+++ packages/ui/themes/src/AstralEDOTheme.js
@@ -1,6 +1,27 @@
 import AstralTheme from './AstralTheme';
 
 const astralEDOThemeOptions = {
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        'div, table, nav, aside, ul, span, li': {
+          '&::-webkit-scrollbar': {
+            width: '5px',
+            height: '5px',
+            background: 'transparent',
+          },
+          '&::-webkit-scrollbar-track': {
+            borderRadius: '3px',
+            background: 'rgba(0, 0, 0, 0.1)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: '#aaa',
+            borderRadius: '2px',
+          },
+        },
+      },
+    },
+  },
   palette: {
     action: {
       hover: '#f3f0fd',
