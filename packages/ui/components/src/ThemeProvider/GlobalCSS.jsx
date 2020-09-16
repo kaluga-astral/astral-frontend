6148ecede6cb40e4433dd7b351a06b63fb8a438d
Pavel Perevezentsev <perevezenzev@gmail.com>
2020-09-16 16:24:00 +0300

    Исправления

--- packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
+++ packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
@@ -28,33 +28,20 @@ export default withStyles(
         boxSizing: 'inherit',
       },
       // '::-webkit-scrollbar': {
-      //   width: '5px',
-      //   height: '5px',
-      //   background: 'transparent',
+      //   backgroundColor: '#fff',
+      //   width: '16px',
       // },
       // '::-webkit-scrollbar-track': {
-      //   borderRadius: '3px',
-      //   background: 'rgba(0, 0, 0, 0.1)',
+      //   backgroundColor: '#fff',
       // },
       // '::-webkit-scrollbar-thumb': {
-      //   background: '#aaa',
-      //   borderRadius: '2px',
+      //   backgroundColor: '#babac0',
+      //   borderRadius: '16px',
+      //   border: '4px solid #fff',
       // },
-      '::-webkit-scrollbar': {
-        backgroundColor: '#fff',
-        width: '16px',
-      },
-      '::-webkit-scrollbar-track': {
-        backgroundColor: '#fff',
-      },
-      '::-webkit-scrollbar-thumb': {
-        backgroundColor: '#babac0',
-        borderRadius: '16px',
-        border: '4px solid #fff',
-      },
-      '::-webkit-scrollbar-button ': {
-        display: 'none',
-      },
+      // '::-webkit-scrollbar-button ': {
+      //   display: 'none',
+      // },
     },
   }),
   { name: 'GlobalCSS' },
