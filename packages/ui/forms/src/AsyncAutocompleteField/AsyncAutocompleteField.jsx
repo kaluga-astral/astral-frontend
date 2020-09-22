541f71f790551191cc62361dca80bc5318f13184
albert <merkel.albert1997@gmail.com>
2020-09-22 18:15:55 +0300

    исправляет reset asyncAutocompleteField

--- packages/ui/forms/src/AsyncAutocompleteField/AsyncAutocompleteField.jsx
+++ packages/ui/forms/src/AsyncAutocompleteField/AsyncAutocompleteField.jsx
@@ -1,4 +1,4 @@
-import { omit } from 'lodash-es';
+import { omit, isEqual } from 'lodash-es';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useField, useFormState } from 'react-final-form';
@@ -16,8 +16,9 @@ const AsyncAutocompleteField = ({
   onChange,
   ...props
 }) => {
-  const { initialValues } = useFormState();
+  const { initialValues, values } = useFormState();
   const initialFieldValue = (initialValues || {})[name] || {};
+  const fieldValue = (values || {})[name] || {};
   const validationFunction = React.useCallback(
     createValidationFunction(required, validate),
     [required, validate],
@@ -36,7 +37,9 @@ const AsyncAutocompleteField = ({
       setValue(null);
       input.onChange(null);
     }
-    onChange(event, newValue);
+    if (onChange) {
+      onChange(event, newValue);
+    }
   };
 
   React.useEffect(() => {
@@ -45,6 +48,13 @@ const AsyncAutocompleteField = ({
       input.onChange((initialFieldValue || {}).value);
     }
   }, [JSON.stringify(initialFieldValue)]);
+
+  React.useEffect(() => {
+    if (isEqual(initialFieldValue, fieldValue)) {
+      setValue(initialFieldValue);
+      input.onChange((initialFieldValue || {}).value);
+    }
+  }, [JSON.stringify(fieldValue)]);
 
   return (
     <AsyncAutocomplete
