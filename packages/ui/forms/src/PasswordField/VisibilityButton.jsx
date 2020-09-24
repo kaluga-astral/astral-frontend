6fef0e6e2e74c7d765bf474bb2b11ffe0384a903
deniskorokhodkin <korokhodkindenis@gmail.com>
2020-09-24 08:44:44 +0300

    fix(PasswordField) исправляет расползания иконок на весь экран

--- packages/ui/forms/src/PasswordField/VisibilityButton.jsx
+++ packages/ui/forms/src/PasswordField/VisibilityButton.jsx
@@ -1,26 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { IconButton } from '@astral-frontend/components';
-import { makeStyles } from '@astral-frontend/styles';
 import { VisibilityIcon, VisibilityOffIcon } from '@astral-frontend/icons';
 
-const useStyles = makeStyles({
-  icon: {
-    width: '100%',
-    height: '100%',
-  },
-});
-
 const VisibilityButton = ({ isVisible, onClick }) => {
-  const classes = useStyles();
-
   return (
     <IconButton size="small" onClick={onClick}>
-      {isVisible ? (
-        <VisibilityIcon className={classes.icon} />
-      ) : (
-        <VisibilityOffIcon className={classes.icon} />
-      )}
+      {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
     </IconButton>
   );
 };
