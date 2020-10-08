ea8955cb9586398b2734fcb7b49e6e2ee36824ad
Pavel Perevezentsev <perevezenzev@gmail.com>
2020-10-08 14:54:02 +0300

    Исправление

--- packages/ui/validations/src/mustBeEmail/mustBeEmail.js
+++ packages/ui/validations/src/mustBeEmail/mustBeEmail.js
@@ -1,4 +1,15 @@
 const emailRegExp = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,5}$/;
 
-export default value =>
-  emailRegExp.test(value) ? null : 'Email имеет некорректный формат';
+export default value => {
+  if (!emailRegExp.test(value)) {
+    return 'Email имеет некорректный формат';
+  }
+
+  const [username] = value.split('@') ?? [];
+
+  if (username.length > 64) {
+    return 'Email имеет некорректный формат';
+  }
+
+  return null;
+};
