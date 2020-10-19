3e4e9aa7b5b253bd48d9383560a13544fd8473be
Andrey Potyomkin <ipad_nw@icloud.com>
2020-10-19 17:42:44 +0300

    Для чекбокса добавлен опциональный label, поправлен цвет checked иконки

--- packages/ui/components/src/Checkbox/Checkbox.jsx
+++ packages/ui/components/src/Checkbox/Checkbox.jsx
@@ -1,13 +1,15 @@
-import cn from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import { Checkbox as MuiCheckbox } from '@astral-frontend/core';
 import { makeStyles } from '@astral-frontend/styles';
 import { CheckedRectIcon, UncheckedRectIcon } from '@astral-frontend/icons';
 
+import FormControlLabel from '../FormControlLabel';
+
 const useStyles = makeStyles(
-  () => ({
+  theme => ({
     root: {
       display: 'inline-flex',
     },
@@ -19,21 +21,54 @@ const useStyles = makeStyles(
     checkedIcon: {
       width: 20,
       height: 20,
+      fill: theme.palette.primary.main,
+
+      '& :hover': {
+        fill: theme.palette.primary.dark,
+      },
     },
   }),
   { name: 'Checkbox' },
 );
 
-const Checkbox = ({ className, ...props }) => {
+const Checkbox = ({
+  disabled,
+  checked,
+  label,
+  labelPlacement,
+  className,
+  helperText,
+  error,
+  value,
+  onChange,
+  onBlur,
+  onFocus,
+  ...props
+}) => {
   const classes = useStyles();
 
-  return (
+  const ControlCheckbox = (
     <MuiCheckbox
+      value={String(value)}
       color="primary"
-      {...props}
-      className={cn(classes.root, className)}
+      className={classes.checkbox}
       icon={<UncheckedRectIcon className={classes.uncheckedIcon} />}
       checkedIcon={<CheckedRectIcon className={classes.checkedIcon} />}
+    />
+  );
+
+  return (
+    <FormControlLabel
+      {...props}
+      checked={checked}
+      disabled={disabled}
+      label={label}
+      labelPlacement={labelPlacement}
+      className={cn(className, classes.root)}
+      control={ControlCheckbox}
+      onChange={onChange}
+      onBlur={onBlur}
+      onFocus={onFocus}
     />
   );
 };
@@ -42,8 +77,31 @@ Checkbox.defaultProps = {
   className: null,
 };
 
+Checkbox.defaultProps = {
+  disabled: false,
+  checked: false,
+  labelPlacement: 'end',
+  className: null,
+  helperText: null,
+  label: '',
+  error: null,
+  onBlur: null,
+  onFocus: null,
+};
+
 Checkbox.propTypes = {
+  disabled: PropTypes.bool,
+  checked: PropTypes.bool,
+  labelPlacement: PropTypes.oneOf(['end', 'start', 'top', 'bottom']),
+  error: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   className: PropTypes.string,
+  helperText: PropTypes.string,
+  label: PropTypes.node,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  // eslint-disable-next-line
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Checkbox;
