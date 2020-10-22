6e1e7e6f6e808bd97f52ce8734c89d5a485c0b0b
Dima Denisov <2denisov@bk.ru>
2020-10-22 11:57:26 +0400

    remove unnecessary code

--- packages/ui/validations/src/mustBeEmail/mustBeEmail.js
+++ packages/ui/validations/src/mustBeEmail/mustBeEmail.js
@@ -1,5 +1,4 @@
 const emailRegExp = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,5}$/;
-const domenZoneRegExp = /\.\w$/;
 
 export default value => {
   if (!emailRegExp.test(value)) {
@@ -7,9 +6,8 @@ export default value => {
   }
 
   const [username] = value.split('@') ?? [];
-  const [domenZone = ''] = value.match(domenZoneRegExp) ?? [];
 
-  if (username.length > 64 || domenZone.length > 5) {
+  if (username.length > 64) {
     return 'Email имеет некорректный формат';
   }
 
