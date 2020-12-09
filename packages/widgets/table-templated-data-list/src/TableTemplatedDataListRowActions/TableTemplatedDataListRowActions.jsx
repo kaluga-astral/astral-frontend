7676146279a52b9d56d6a651b1f1675853274dc8
albert <merkel.albert1997@gmail.com>
2020-12-09 12:30:59 +0300

    исправляет анимацию RawActions

--- packages/widgets/table-templated-data-list/src/TableTemplatedDataListRowActions/TableTemplatedDataListRowActions.jsx
+++ packages/widgets/table-templated-data-list/src/TableTemplatedDataListRowActions/TableTemplatedDataListRowActions.jsx
@@ -2,11 +2,24 @@ import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { fade } from '@astral-frontend/core';
 import { FlexContainer } from '@astral-frontend/components';
 import { makeStyles } from '@astral-frontend/styles';
+import {
+  DataListContext,
+  DataListItemContext,
+} from '@astral-frontend/data-list';
 
 const useStyles = makeStyles(
   theme => ({
+    '@keyframes highlight': {
+      '0%': {
+        backgroundColor: fade(theme.palette.primary.light, 0.5),
+      },
+      '100%': {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
     root: {
       position: 'absolute',
       right: 0,
@@ -16,6 +29,9 @@ const useStyles = makeStyles(
       backgroundColor: theme.palette.common.white,
       margin: theme.spacing(1, 0),
       borderLeft: `1px solid ${theme.palette.primary.light}`,
+    },
+    highlighted: {
+      animation: `$highlight 3500ms ${theme.transitions.easing.easeInOut}`,
     },
   }),
   { name: 'TableTemplatedDataListRowActions' },
@@ -27,11 +43,18 @@ export const TableTemplatedDataListRowActions = ({
   ...props
 }) => {
   const classes = useStyles();
+  const { lastViewedItem } = React.useContext(DataListContext);
+  const { data } = React.useContext(DataListItemContext);
+  const highlighted = React.useMemo(() => {
+    return data.id === lastViewedItem?.id;
+  }, [data.id, lastViewedItem?.id]);
 
   return (
     <FlexContainer
       alignItems="center"
-      className={cn(className, classes.root)}
+      className={cn(className, classes.root, {
+        [classes.highlighted]: highlighted,
+      })}
       {...props}
     >
       {children}
