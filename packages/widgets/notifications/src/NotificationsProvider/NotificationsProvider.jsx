7a346a4af36b76e26f7fb4d6f981e15eeff684b9
Dima Denisov <2denisov@bk.ru>
2020-12-18 01:19:25 +0300

    add even more props to NotificationProvider

--- packages/widgets/notifications/src/NotificationsProvider/NotificationsProvider.jsx
+++ packages/widgets/notifications/src/NotificationsProvider/NotificationsProvider.jsx
@@ -7,19 +7,21 @@ import NotificationsContext from '../NotificationsContext';
 const DEFAULT_NOTIFICATION_DURATION = 5000;
 
 const NotificationsProvider = ({
+  anchorOrigin,
   autoHideDuration,
   children,
   darkMode,
   marker,
+  maxSnack,
   progressLine,
 }) => (
   <NotificationsContext.Provider
     value={{ autoHideDuration, darkMode, marker, progressLine }}
   >
     <SnackbarProvider
-      maxSnack={3}
+      maxSnack={maxSnack}
+      anchorOrigin={anchorOrigin}
       autoHideDuration={null}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
     >
       {children}
     </SnackbarProvider>
@@ -27,17 +29,24 @@ const NotificationsProvider = ({
 );
 
 NotificationsProvider.defaultProps = {
+  anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
   autoHideDuration: DEFAULT_NOTIFICATION_DURATION,
   darkMode: false,
   marker: false,
+  maxSnack: 12,
   progressLine: false,
 };
 
 NotificationsProvider.propTypes = {
+  anchorOrigin: PropTypes.shape({
+    vertical: PropTypes.oneOf(['top', 'bottom']),
+    horizontal: PropTypes.oneOf(['left', 'center', 'right']),
+  }),
   autoHideDuration: PropTypes.number,
   children: PropTypes.node.isRequired,
   darkMode: PropTypes.bool,
   marker: PropTypes.bool,
+  maxSnack: PropTypes.number,
   progressLine: PropTypes.bool,
 };
 
