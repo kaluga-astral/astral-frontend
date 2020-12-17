17758f834ed579c3f3e758ace5c6038dae616464
Dima Denisov <2denisov@bk.ru>
2020-12-17 17:36:08 +0300

    revert bd6f5476eb7895da1f2b553fb4ec78e402ba7fbb

--- packages/widgets/notifications/src/useNotification.jsx
+++ packages/widgets/notifications/src/useNotification.jsx
@@ -2,65 +2,18 @@
 import { useSnackbar } from 'notistack';
 import React from 'react';
 
+import NotificationsMessage from './NotificationsMessage';
 import NotificationsSystemNotification from './NotificationsSystemNotification';
-import Notification from './Notification';
 
 export const useNotification = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const enqueueCustomNotification = options => {
-    return enqueueSnackbar('', options);
-  };
-  const enqueueNotification = ({
-    variant = 'info',
-    title,
-    message,
-    progressLine,
-    autoHideDuration,
-    darkMode = false,
-    persist = false,
-    marker,
-  }) => {
-    return enqueueSnackbar('', {
-      persist: true,
-      content: key => (
-        <Notification
-          id={key}
-          {...{
-            variant,
-            title,
-            message,
-            progressLine,
-            autoHideDuration,
-            darkMode,
-            persist,
-            marker,
-          }}
-        />
-      ),
-    });
-  };
-  const enqueueInfoNotification = props => {
-    return enqueueNotification({ title: 'Успешно', ...props, variant: 'info' });
-  };
-  const enqueueSuccessNotification = props => {
-    return enqueueNotification({
-      title: 'Успешно',
-      ...props,
-      variant: 'success',
-    });
-  };
-  const enqueueErrorNotification = props => {
-    return enqueueNotification({ title: 'Ошибка', ...props, variant: 'error' });
-  };
 
   return React.useMemo(
     () => ({
       closeNotification: closeSnackbar,
-      enqueueCustomNotification,
-      enqueueNotification,
-      enqueueInfoNotification,
-      enqueueSuccessNotification,
-      enqueueErrorNotification,
+      enqueueCustomNotification(options) {
+        return enqueueSnackbar('', options);
+      },
       enqueueSystemNotification({ options, Component, ...props }) {
         return enqueueSnackbar('', {
           persist: true,
@@ -77,139 +30,188 @@ export const useNotification = () => {
           ...options,
         });
       },
-      // enqueueSuccessNotification({
-      //   title = 'Успешно',
-      //   message = '',
-      //   duration = null,
-      //   progressLine = true,
-      // }) {
-      //   enqueueSnackbar('', {
-      //     variant: 'success',
-      //     content: key => (
-      //       <NotificationsMessage
-      //         id={key}
-      //         variant="success"
-      //         title={title}
-      //         // TODO 29354
-      //         message={
-      //           typeof message === 'string'
-      //             ? message.replace('GraphQL error: ', '')
-      //             : message
-      //         }
-      //         duration={duration}
-      //         progressLine={progressLine}
-      //       />
-      //     ),
-      //   });
-      // },
-      // enqueueErrorNotification({
-      //   title = 'Ошибка',
-      //   message = '',
-      //   duration = null,
-      //   progressLine = true,
-      //   ...options
-      // }) {
-      //   enqueueSnackbar('', {
-      //     ...options,
-      //     variant: 'error',
-      //     content: key => (
-      //       <NotificationsMessage
-      //         id={key}
-      //         variant="error"
-      //         title={title}
-      //         // TODO 29354
-      //         message={
-      //           typeof message === 'string'
-      //             ? message.replace('GraphQL error: ', '')
-      //             : message
-      //         }
-      //         duration={duration}
-      //         progressLine={progressLine}
-      //       />
-      //     ),
-      //   });
-      // },
-      // enqueueInfoNotification({
-      //   title = 'Успешно',
-      //   message = '',
-      //   duration = null,
-      //   progressLine = true,
-      //   ...options
-      // }) {
-      //   enqueueSnackbar('', {
-      //     variant: 'info',
-      //     content: key => (
-      //       <NotificationsMessage
-      //         id={key}
-      //         variant="info"
-      //         title={title}
-      //         // TODO 29354
-      //         message={
-      //           typeof message === 'string'
-      //             ? message.replace('GraphQL error: ', '')
-      //             : message
-      //         }
-      //         duration={duration}
-      //         progressLine={progressLine}
-      //       />
-      //     ),
-      //     ...options,
-      //   });
-      // },
-      // enqueueDeleteNotification({
-      //   title = 'Удаление',
-      //   message = '',
-      //   duration = null,
-      //   progressLine = true,
-      // }) {
-      //   enqueueSnackbar('', {
-      //     variant: 'delete',
-      //     content: key => (
-      //       <NotificationsMessage
-      //         id={key}
-      //         variant="delete"
-      //         title={title}
-      //         // TODO 29354
-      //         message={
-      //           typeof message === 'string'
-      //             ? message.replace('GraphQL error: ', '')
-      //             : message
-      //         }
-      //         duration={duration}
-      //         progressLine={progressLine}
-      //       />
-      //     ),
-      //   });
-      // },
-      // enqueueNotification({
-      //   variant = 'info',
-      //   title,
-      //   message,
-      //   progressLine,
-      //   autoHideDuration,
-      //   darkMode = false,
-      //   persist = false,
-      //   marker,
-      // }) {
-      //   enqueueSnackbar('', {
-      //     persist: true,
-      //     content: key => (
-      //       <Notification
-      //         id={key}
-      //         {...{
-      //           variant,
-      //           title,
-      //           message,
-      //           progressLine,
-      //           autoHideDuration,
-      //           darkMode,
-      //           persist,
-      //           marker,
-      //         }}
-      //       />
-      //     ),
-      //   });
-      // },
+      enqueueSuccessNotification({
+        title = 'Успешно',
+        message = '',
+        duration = null,
+        progressLine = true,
+      }) {
+        enqueueSnackbar('', {
+          variant: 'success',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="success"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+            />
+          ),
+        });
+      },
+      enqueueErrorNotification({
+        title = 'Ошибка',
+        message = '',
+        duration = null,
+        progressLine = true,
+        ...options
+      }) {
+        enqueueSnackbar('', {
+          ...options,
+          variant: 'error',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="error"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+            />
+          ),
+        });
+      },
+      enqueueInfoNotification({
+        title = 'Успешно',
+        message = '',
+        duration = null,
+        progressLine = true,
+        ...options
+      }) {
+        enqueueSnackbar('', {
+          variant: 'info',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="info"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+            />
+          ),
+          ...options,
+        });
+      },
+      enqueueSuccessTimerNotification({
+        title = 'Успешно',
+        message = '',
+        duration = null,
+        progressLine = false,
+      }) {
+        enqueueSnackbar('', {
+          variant: 'success',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="success"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+              timer
+            />
+          ),
+        });
+      },
+      enqueueErrorTimerNotification({
+        title = 'Ошибка',
+        message = '',
+        duration = null,
+        progressLine = false,
+      }) {
+        enqueueSnackbar('', {
+          variant: 'error',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="error"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+              timer
+            />
+          ),
+        });
+      },
+      enqueueInfoTimerNotification({
+        title = 'Успешно',
+        message = '',
+        duration = null,
+        progressLine = false,
+      }) {
+        enqueueSnackbar('', {
+          variant: 'info',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="info"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+              timer
+            />
+          ),
+        });
+      },
+      enqueueDeleteNotification({
+        title = 'Удаление',
+        message = '',
+        duration = null,
+        progressLine = true,
+      }) {
+        enqueueSnackbar('', {
+          variant: 'delete',
+          content: key => (
+            <NotificationsMessage
+              id={key}
+              variant="delete"
+              title={title}
+              // TODO 29354
+              message={
+                typeof message === 'string'
+                  ? message.replace('GraphQL error: ', '')
+                  : message
+              }
+              duration={duration}
+              progressLine={progressLine}
+            />
+          ),
+        });
+      },
     }),
     [],
   );
