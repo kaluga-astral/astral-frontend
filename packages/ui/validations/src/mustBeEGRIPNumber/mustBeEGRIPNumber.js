dff08f4148f7327c920e4d797d55d9f7a9ee00f4
Перевезенцев Павел Александрович <perevezentsev_pa@astralnalog.ru>
2021-02-25 07:18:56 +0000

    Исправляет валидацию поля номер "Номер и серия свидетельства, подтверждающее внесение в ЕГРИП"

--- packages/ui/validations/src/mustBeEGRIPNumber/mustBeEGRIPNumber.js
+++ packages/ui/validations/src/mustBeEGRIPNumber/mustBeEGRIPNumber.js
@@ -1,4 +1,4 @@
-const egripNumberRegExp = /^\d{2} \d{12}$/;
+const egripNumberRegExp = /^\d{2} \d{8,12}$/;
 
 export default function mustBeEGRIPNumber(value) {
   if (egripNumberRegExp.test(value)) {
