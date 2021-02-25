351ea76ca7b25da0e12812f57a31b2531738ba3a
Перевезенцев Павел Александрович <perevezentsev_pa@astralnalog.ru>
2021-02-25 07:34:42 +0000

    Исправляет валидацию поля "Серия и номер свидетельства о регистрации ИП"

--- packages/ui/validations/src/mustBeEGRIPNumber/mustBeEGRIPNumber.js
+++ packages/ui/validations/src/mustBeEGRIPNumber/mustBeEGRIPNumber.js
@@ -1,7 +1,7 @@
 const egripNumberRegExp = /^\d{2} \d{8,12}$/;
 
 export default function mustBeEGRIPNumber(value) {
-  if (egripNumberRegExp.test(value)) {
+  if (egripNumberRegExp.test(value.trim())) {
     return null;
   }
 
