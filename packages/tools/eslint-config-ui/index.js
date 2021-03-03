b3cfd56c2b728120e55fba468994f097ec815764
Andrey Potyomkin <and_tem@icloud.com>
2021-03-03 16:29:26 +0300

    Добавлены новые правила eslint

--- packages/tools/eslint-config-ui/index.js
+++ packages/tools/eslint-config-ui/index.js
@@ -1,11 +1,20 @@
 module.exports = {
   extends: ['airbnb', 'plugin:prettier/recommended'],
   parser: 'babel-eslint',
+  plugins: ['prettier', 'jest'],
   env: {
     browser: true,
     jest: true,
   },
   rules: {
+    // проблема при экспорте одиночной константы
+    // TODO: вынести на обсуждение по style guid
+    'import/prefer-default-export': 'off',
+    quotes: ['error', 'single'],
+    // есть неразрешимая ошибка, когда в jsx объявляется стрелочная функция без явного указания return
+    // TODO: вынести на обсуждение по style guid
+    'react/jsx-curly-newline': 'off',
+    // смотрим, чтобы в линки не прокидывали лишнего, например onClick
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -23,6 +32,32 @@ module.exports = {
       {
         html: 'enforce',
         custom: 'ignore',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['internal'],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'object',
+          'index',
+        ],
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
       },
     ],
   },
