92804b4512652e959fe7206319b30b5e73900fa2
albert <merkel.albert1997@gmail.com>
2021-03-19 12:24:34 +0300

    settlement in address

--- packages/ui/validations/src/mustBeAddress/mustBeAddress.js
+++ packages/ui/validations/src/mustBeAddress/mustBeAddress.js
@@ -1,9 +1,9 @@
-export const EMPTY_CITY_ERROR_MESSAGE =
+export const EMPTY_CITY_OR_SETTLEMENT_ERROR_MESSAGE =
   'Неверный адрес. Выберите населенный пункт';
 
 const mustBeAddress = value => {
-  if (!value.city) {
-    return EMPTY_CITY_ERROR_MESSAGE;
+  if (!value.city && !value.settlement) {
+    return EMPTY_CITY_OR_SETTLEMENT_ERROR_MESSAGE;
   }
 
   return null;
