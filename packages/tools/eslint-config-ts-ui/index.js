658231c536bb9dad109bd25056358e366a052f87
aleksandrplugar <plugar_as@astralnalog.ru>
2021-04-12 12:23:52 +0300

    Правки по ПР

--- packages/tools/eslint-config-ts-ui/index.js
+++ packages/tools/eslint-config-ts-ui/index.js
@@ -38,68 +38,6 @@ module.exports = {
     'import/order': [
       'error',
       {
-        pathGroups: [
-          {
-            pattern: '@foundation/gql-dto/**',
-            group: 'external',
-            position: 'after',
-          },
-          {
-            pattern: '@foundation/cypress/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@foundation/core/components/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@foundation/core/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@foundation/entities/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@foundation/modules/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@foundation/interactors/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'shared/**',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'entities/**',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'modules/**',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'config/**',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'images/**',
-            group: 'internal',
-            position: 'after',
-          },
-        ],
         'newlines-between': 'always',
         pathGroupsExcludedImportTypes: ['internal'],
         groups: [
