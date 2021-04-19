2c2e49c4a13cdb638cd92f95fa423d15cd2ae81e
Dima Denisov <2denisov@bk.ru>
2021-04-19 18:14:04 +0300

    #fix убирает магические цифры в константы

--- packages/utils/src/declensionOfNumber/declensionOfNumber.js
+++ packages/utils/src/declensionOfNumber/declensionOfNumber.js
@@ -1,7 +1,7 @@
 /**
  * Функция для подбора нужного имени в зависимости от числа
  * @param {number} number число
- * @param {string[]} titles массив имен
+ * @param {string[]} titles массив имен [имя, когда number === 1, имя, когда number === 2, имя, когда number === 5]
  * @returns число с нужнем именем
  * @example
  * // returns 5 документов
@@ -9,10 +9,10 @@
  */
 export function declensionOfNumber(number, titles) {
   const cases = [2, 0, 1, 1, 1, 2];
+  const isSecondTitle = number % 100 > 4 && number % 100 < 20;
+  const isRemainderLessThanFive = number % 10 < 5;
 
   return titles[
-    number % 100 > 4 && number % 100 < 20
-      ? 2
-      : cases[number % 10 < 5 ? number % 10 : 5]
+    isSecondTitle ? 2 : cases[isRemainderLessThanFive ? number % 10 : 5]
   ];
 }
