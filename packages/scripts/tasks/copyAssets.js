83d13d741b0605517ac5d828b1e43b828936ee9b
Andrey Potyomkin <and_tem@icloud.com>
2021-05-11 17:15:42 +0300

    изменены настройки ts. Сделано так, чтобы при билде ts пакетов, в lib попадали типы

---
+++ packages/scripts/tasks/copyAssets.js
@@ -0,0 +1,27 @@
+const copy = require('recursive-copy');
+
+const copyAssets = () => {
+  copy('./src', './lib', {
+    filter: [
+      '**/*.woff2',
+      '**/*.otf',
+      '**/*.ttf',
+      '**/*.woff',
+      '**/*.css',
+      '**/*.svg',
+      '**/*.png',
+      '**/*.jpg',
+    ],
+    overwrite: true,
+    expand: true,
+    dot: true,
+    junk: true,
+  }).catch(error => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    process.exit(1);
+  });
+};
+
+module.exports = { copyAssets };
