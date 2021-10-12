640ca0f2a9dda0da5e72d773eccc55598b51fe13
Albert Merkel <merkel.albert1997@gmail.com>
2021-10-12 13:49:15 +0300

    fix Tooltip

--- packages/ui/components/src/Tooltip/Tooltip.jsx
+++ packages/ui/components/src/Tooltip/Tooltip.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip as MuiTooltip } from '@astral-frontend/core';
 
-const Tooltip = ({ children, ...props }) => {
+const Tooltip = ({ children, interactive, ...props }) => {
   return (
-    <MuiTooltip arrow enterDelay={500} {...props}>
+    <MuiTooltip
+      disableInteractive={!interactive}
+      arrow
+      enterDelay={500}
+      {...props}
+    >
       <div>{children}</div>
     </MuiTooltip>
   );
 };
 
-Tooltip.defaultProps = {};
+Tooltip.defaultProps = {
+  interactive: false,
+};
 
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
+  interactive: PropTypes.bool,
 };
 
 export default Tooltip;
