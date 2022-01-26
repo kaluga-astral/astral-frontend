646da140380435d9a30529105c54bee0c06e37c8
Perevezentsev Pavel <perevezenzev@gmail.com>
2022-01-26 13:51:50 +0300

    Исправляет наличие ненужного горизонтального скрола

--- packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
+++ packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
@@ -12,7 +12,7 @@ export default withStyles(
         fontSize: theme.typography.htmlFontSize,
       },
       'html, body, #root': {
-        width: '100vw',
+        // width: '100vw',
         height: '100vh',
         margin: 0,
         padding: 0,
