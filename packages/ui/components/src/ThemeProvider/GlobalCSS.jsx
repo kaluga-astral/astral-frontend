12db9065dd1052fb671e180cf431d16c63d98fd8
Perevezentsev Pavel <perevezenzev@gmail.com>
2022-01-26 13:59:24 +0300

    Удаляет неиспользуемый код

--- packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
+++ packages/ui/components/src/ThemeProvider/GlobalCSS.jsx
@@ -12,7 +12,6 @@ export default withStyles(
         fontSize: theme.typography.htmlFontSize,
       },
       'html, body, #root': {
-        // width: '100vw',
         height: '100vh',
         margin: 0,
         padding: 0,
@@ -27,11 +26,9 @@ export default withStyles(
       '*, *::before, *::after': {
         boxSizing: 'inherit',
       },
-
       '*::-webkit-scrollbar': {
         width: '4px',
       },
-
       '*::-webkit-scrollbar-thumb': {
         borderRadius: '8px',
         backgroundColor: theme.palette.gray[300],
