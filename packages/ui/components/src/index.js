6fc09e27b4618a05cdc1dbbaf4619d7e16a30a4c
Sergey Varlamov <varlamov_ss@astralnalog.ru>
2022-09-09 16:11:12 +0500

    feat: перемешает DataListFilters из components в table-templated-data-list

--- packages/ui/components/src/index.js
+++ packages/ui/components/src/index.js
@@ -110,4 +110,3 @@ export { default as RadioGroupField } from './RadioGroupField';
 export { default as DatePicker, DatePickerUtilsProvider } from './DatePicker';
 export { default as useDropzone } from './useDropzone';
 export { default as usePopupStateManager } from './usePopupStateManager';
-export { default as DataListFilters} from './DataListFilters'
