7c2fd285e5b9df5392b13828cf7423f21e52bdaa
Alexey Malyshev <lordniken@mail.ru>
2022-09-19 15:23:01 +0300

    Добавлен шрифт Golos

--- packages/ui/fonts/src/fontsConfigs.js
+++ packages/ui/fonts/src/fontsConfigs.js
@@ -1,7 +1,9 @@
 import MANROPE_FONTS_CONFIG from './Manrope';
 import ROBOTO_FONTS_CONFIG from './Roboto';
+import GOLOS_FONTS_CONFIG from './Golos';
 
 export default {
   [MANROPE_FONTS_CONFIG.name]: MANROPE_FONTS_CONFIG.connectInfo,
   [ROBOTO_FONTS_CONFIG.name]: ROBOTO_FONTS_CONFIG.connectInfo,
+  [GOLOS_FONTS_CONFIG.name]: GOLOS_FONTS_CONFIG.connectInfo,
 };
